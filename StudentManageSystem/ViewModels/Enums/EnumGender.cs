f31052c601dc85a64a7f64b2630ff1ea946afcd6
h_gxi <h_gxi@foxmail.com>
2020-04-23 19:10:43 +0800

    封装

---
+++ StudentManageSystem/ViewModels/Enums/EnumGender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentManageSystem.ViewModels.Enums
+{
+    /// <summary>
+    /// 性别
+    /// </summary>
+    public enum EnumGender
+    {
+        [Display(Name = "未知")]
+        UnKnown = -1,
+        /// <summary>
+        /// 男
+        /// </summary>
+        [Display(Name = "男")]
+        Man,
+        /// <summary>
+        /// 女
+        /// </summary>
+        [Display(Name = "女")]
+        Woman
+    }
+}
