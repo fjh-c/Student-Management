c9a95b9f7dbcaa1da7b1eab339fd54a3c24fa35f
h_gxi <h_gxi@foxmail.com>
2020-04-26 22:33:38 +0800

    添加描述

--- Student.DTO/StudentInfoDTO.cs
+++ Student.DTO/StudentInfoDTO.cs
@@ -2,70 +2,47 @@
 using Student.Model.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Student.DTO
 {
     public class StudentInfoDTO
     {
-        /// <summary>
-        /// 编号
-        /// </summary>
+        [Description("学生编号")]
         public long Id { get; set; }
-        /// <summary>
-        /// 激活状态
-        /// </summary>
+        [Description("激活状态")]
         public int Status { get; set; }
-        /// <summary>
-        /// 学生姓名
-        /// </summary>
+        [Description("学生姓名")]
+        [Required]
         public string Name { get; set; }
-        /// <summary>
-        /// 学生性别
-        /// </summary>
+        [Description("学生性别")]
         public int Gender { get; set; }
-        /// <summary>
-        /// 学生民族
-        /// </summary>
+        [Description("学生民族")]
         public int Nation { get; set; }
-        /// <summary>
-        /// 入学时间
-        /// </summary>
+        [Description("入学时间")]
+        [DataType(DataType.Date)]
         public DateTime EnrollmentDT { get; set; }
-        /// <summary>
-        /// 部门ID
-        /// </summary>
+        [Description("学生部门")]
         public int DepartId { get; set; }
-        /// <summary>
-        /// 学生电话
-        /// </summary>
+        [Description("学生电话")]
+        [Phone]
         public string Phone { get; set; }
-        /// <summary>
-        /// 学生邮箱
-        /// </summary>
+        [Description("学生邮箱")]
+        [EmailAddress]
         public string Email { get; set; }
-        /// <summary>
-        /// 身份证号码
-        /// </summary>
+        [Description("身份证号码")]
+        [Required]
         public string PersonId { get; set; }
-        /// <summary>
-        /// 学生地址
-        /// </summary>
+        [Description("学生地址")]
         public string Address { get; set; }
-        /// <summary>
-        /// 学生照片
-        /// </summary>
+        [Description("学生照片")]
         public string Photos { get; set; }
-        /// <summary>
-        /// 性别
-        /// </summary>
+        [Description("学生性别")]
         public string GenderName { get; set; }
-        /// <summary>
-        /// 民族
-        /// </summary>
+        [Description("民族显示")]
         public string NationName { get; set; }
-        /// <summary>
-        /// 部门名称
-        /// </summary>
+        [Description("部门显示")]
         public string DepartName { get; set; }
     }
 }
