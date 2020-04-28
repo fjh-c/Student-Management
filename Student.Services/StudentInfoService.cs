c356dcacbf105a7914761ac1704160ea5c73c362
hgx <h_gxi@foxmail.com>
2020-04-28 15:21:08 +0800

    封装

--- Student.Services/StudentInfoService.cs
+++ Student.Services/StudentInfoService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Student.DTO;
 using Student.IServices;
 using Student.Model;
@@ -15,18 +16,20 @@ namespace Student.Services
 {
     public class StudentInfoService: IStudentInfoService, IDependency
     {
+        private readonly ILogger<StudentInfoService> _logger;
         private readonly Lazy<IMapper> _mapper;
         private readonly Lazy<IRepository<StudentInfo>> repStudentInfo;
         private readonly Lazy<IRepository<Depart>> repDepart;
 
         public IUnitOfWork UnitOfWork { get; }
 
-        public StudentInfoService(Lazy<IMapper> mapper, IUnitOfWork unitOfWork, 
+        public StudentInfoService(Lazy<IMapper> mapper, IUnitOfWork unitOfWork, ILogger<StudentInfoService> logger,
             Lazy<IRepository<StudentInfo>> repStudentInfo, 
             Lazy<IRepository<Depart>> repDepart)
         {
-            this._mapper = mapper;
-            this.UnitOfWork = unitOfWork;
+            _logger = logger;
+            _mapper = mapper;
+            UnitOfWork = unitOfWork;
             this.repStudentInfo = repStudentInfo;
             this.repDepart = repDepart;
         }
@@ -50,6 +53,7 @@ namespace Student.Services
             var dept = repDepart.Value.GetById(entity.DepartId);
             if (dept == null || dept.DeptType != Model.Enums.EnumDeptType.classes)
             {
+                _logger.LogError($"error：Departid {entity.DepartId} does not exist or the EnumDeptType is not classes");
                 return ResultModel.Failed("error：Departid does not exist or the EnumDeptType is not classes");
             }
             await repStudentInfo.Value.InsertAsync(entity);
@@ -58,7 +62,8 @@ namespace Student.Services
             {
                 return ResultModel.Success(entity);
             }
-            return ResultModel.Failed("error：Insert Save failed");
+            _logger.LogError($"error：Insert Save failed");
+            return ResultModel.Failed("添加失败");
         }
 
         public async Task<IResultModel> Update(StudentInfoDTO model)
@@ -67,13 +72,15 @@ namespace Student.Services
             var entity = await repStudentInfo.Value.GetByIdAsync(model.Id);
             if(entity == null)
             {
-                return ResultModel.Failed("error：entity Id does not exist");
+                _logger.LogError($"error：entity Id {model.Id} does not exist");
+                return ResultModel.NotExists;
             }
             //外键判断
             var dept = repDepart.Value.GetById(model.DepartId);
             if (dept == null || dept.DeptType != Model.Enums.EnumDeptType.classes)
             {
-                return ResultModel.Failed("error：Departid does not exist or the EnumDeptType is not classes");
+                _logger.LogError($"error：Departid {model.DepartId} does not exist or the EnumDeptType is not classes");
+                return ResultModel.Failed("外键不存在，或部门必须指定班级");
             }
             _mapper.Value.Map(model, entity);
             repStudentInfo.Value.Update(entity);
@@ -82,7 +89,8 @@ namespace Student.Services
             {
                 return ResultModel.Success(entity);
             }
-            return ResultModel.Failed("error：Insert Save failed");
+            _logger.LogError($"error：Update Save failed");
+            return ResultModel.Failed("修改失败");
         }
 
         public async Task<IResultModel> Delete(long id)
@@ -91,7 +99,8 @@ namespace Student.Services
             var entity = await repStudentInfo.Value.GetByIdAsync(id);
             if (entity == null)
             {
-                return ResultModel.Failed("error：entity Id does not exist");
+                _logger.LogError($"error：entity Id {id} does not exist");
+                return ResultModel.NotExists;
             }
             //软删除
             if(entity.Deleted == 0)
@@ -106,9 +115,10 @@ namespace Student.Services
             }
             if (await UnitOfWork.SaveChangesAsync() > 0)
             {
-                return ResultModel.Success("Delete succeeded");
+                return ResultModel.Success();
             }
-            return ResultModel.Failed("error：Delete failed");
+            _logger.LogError($"error：Delete failed");
+            return ResultModel.Failed("删除失败");
         }
     }
 }
