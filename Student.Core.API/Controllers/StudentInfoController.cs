fefaa60a30cfed16502023c02e288c506335cea0
hgx <h_gxi@foxmail.com>
2020-04-29 10:12:14 +0800

    封装

--- Student.Core.API/Controllers/StudentInfoController.cs
+++ Student.Core.API/Controllers/StudentInfoController.cs
@@ -75,7 +75,7 @@ namespace Student.Core.API.Controllers
 
         [Description("批量删除学生信息")]
         [HttpDelete]
-        public async Task<IResultModel> DeleteAll([Required]IList<long> ids)
+        public async Task<IResultModel> DeleteAll([FromBody]IList<long> ids)
         {
             _logger.LogDebug("批量删除学生信息");
             foreach (var id in ids)
