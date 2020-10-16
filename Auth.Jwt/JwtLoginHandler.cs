d08d108e20fc121115ffa7ba3c34918c6ac8e866
一如既往 <h_gxi@foxmail.com>
2020-10-16 13:17:03 +0800

    获取Auth配置数据库中读取

--- Auth.Jwt/JwtLoginHandler.cs
+++ Auth.Jwt/JwtLoginHandler.cs
@@ -1,35 +1,47 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Student.DTO;
+using Student.IServices;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using yrjw.CommonToolsCore.Attributes;
+using yrjw.ORM.Chimp.Result;
 
 namespace Auth.Jwt
 {
-    [Singleton]
+    [Scoped]
     public class JwtLoginHandler : ILoginHandler
     {
         private readonly ILogger _logger;
+        private readonly IConfigService _configService;
 
-        public JwtLoginHandler(ILogger<JwtLoginHandler> logger)
+        public JwtLoginHandler(ILogger<JwtLoginHandler> logger, IConfigService configService)
         {
             _logger = logger;
+            _configService = configService;
         }
 
         public JwtTokenModel Hand(List<Claim> claims, string extendData)
         {
-            var options = AuthConfigData.AuthConfig.Jwt;
-            var token = Build(claims, options);
+            var _authConfig = new AuthConfigData();
+            var config = _configService.GetValue("Auth").GetAwaiter().GetResult() as ResultModel<ConfigDTO>;
+            if (config.Success && config.Data.Code.EqualsIgnoreCase("Auth"))
+            {
+                _authConfig = config.Data.Value.ToJson<AuthConfigData>();
+            }
+            var jwtConfig = _authConfig.Jwt;
+
+            var token = Build(claims, jwtConfig);
 
             _logger.LogDebug("生成JwtToken：{token}", token);
 
             var model = new JwtTokenModel
             {
                 AccessToken = token,
-                ExpiresIn = options.Expires * 60,
+                ExpiresIn = jwtConfig.Expires * 60,
                 RefreshToken = extendData
             };
 
